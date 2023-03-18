e3260c5e8a3bcaae5836499c0938148fc80e005e
reemsakr <reemsakr060@gmail.com>
2023-03-18 23:12:51 +0200

    redis&bumpApi

--- src/Routes/bumpRoutes.js
+++ src/Routes/bumpRoutes.js
@@ -1,6 +1,6 @@
 const {Router} = require('express')
 const router=Router()
-
+const jwt= require('jsonwebtoken')
 const Bump = require('../Models/bumpModel')
 
 const verifyToken = require('../middleWares/verfyTokenMiddleWare')
@@ -40,6 +40,16 @@ router.post('/add', verifyToken,async (req, res) => {
         })
     }
 })
+const redis=require('redis')
+const redisUrl='redis://127.0.0.1:6379'
+const client=redis.createClient(redisUrl)
+
+client.connect()
+client.on('connect', function() {
+    console.log('redis Connected!')
+})
+
+
 
 router.post('/checkBumps', verifyToken,async (req, res) => {
 
@@ -47,6 +57,16 @@ router.post('/checkBumps', verifyToken,async (req, res) => {
     const long = location.coordinates[0]
     const lat = location.coordinates[1]
     try {
+        
+        const cachedBumpm=await client.get(JSON.stringify(location))
+        
+        if(cachedBumpm)
+        {
+            
+            return res.status(200).send(
+                (JSON.parse(cachedBumpm)).Bumps
+            )}
+        
         const Bumps =await Bump.aggregate([
             {
                 $geoNear: {
@@ -59,13 +79,70 @@ router.post('/checkBumps', verifyToken,async (req, res) => {
                 }   
             }
         ])
-        res.status(200).send(Bumps)
+        const  result={
+            'Bumps':Bumps
+        }
+        client.set(JSON.stringify(location),JSON.stringify(result))
+        client.expire(JSON.stringify(location),60*60*24)
+        res.status(200).send(result.Bumps)
     } catch (err) {
 
         res.status(400).json({
             data:err
         })
     }
+})
+
+
+router.put('/', verifyToken,async (req, res) => {
+    try{
+        const token = req.headers.authorization.split(' ')[1]
+            
+        jwt.verify(token, process.env.TOKEN_SECRET)
+    
+        const id = req.body.bumpId  
+        const bump = await Bump.findById(id)
+        const flag=req.body.flag
+        if(flag=='true')
+        {
+            
+            // eslint-disable-next-line no-unused-vars
+            const updatedBump=await Bump.updateOne(
+                { _id: id },
+                {
+                    $inc: {feedbackCounter:1}
+                
+                }
+            )
+            
+        }
+        else{
+            await Bump.updateOne(
+                { _id: id },
+                {
+                    $inc: {feedbackCounter:-1}
+                }
+            )
+            if(bump.feedbackCounter==2)
+            {
+                
+                bump.deleteOne({_id:id})
+            }
+        }
+        
+        
+        
+        res.status(200).json({
+            data:'success'
+        })
+    
+    }
+    catch (err) {
+        
+        res.status(400).json({
+            data:err})
+    }
+
 })
 
 
